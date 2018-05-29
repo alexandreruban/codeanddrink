4a46bee3430b51608dedfa7b3db67e5782aa6795
Guillaume Legrand <gjlegrandfr@gmail.com>
2018-05-29 14:37:00 +0200

    create simple seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying previous data..."
+Game.destroy_all
+GameMaster.destroy_all
+
+
+file = "db/samples.yml"
+samples = YAML.load(open(file).read)
+
+# Create all game masters
+puts "Creating game masters..."
+game_masters = {}
+game_master_samples = samples["game_masters"]
+game_master_samples.each do |game_master_sample|
+  game_masters[game_master_sample["id"]] = GameMaster.create!(game_master_sample.slice("email", "password"))
+end
+
+# Create all exercises
+puts "Creating exercises..."
+exercises = {}
+exercise_samples = samples["exercises"]
+exercise_samples.each do |exercise_sample|
+  exercises[exercise_sample["id"]] = Exercice.create!(exercise_sample.slice("title", "rules", "specs", "solution"))
+end
+
+# Create all games
+puts "Creating games..."
+games = {}
+game_samples = samples["games"]
+game_samples.each do |game_sample|
+  game = Game.new(game_sample.slice("title", "description", "wifi_network", "wifi_password", "starts_at", "password"))
+  games[game_sample["id"]] = game
+  game.game_master = game_masters[game_sample["game_master_id"]]
+  game.save!
+end
+
+# Create all rounds
+puts "Creating rounds..."
+round_samples = samples["rounds"]
+round_samples.each do |round_sample|
+  round = Round.new
+  round.game = games[round_sample["game_id"]]
+  round.exercice = exercises[round_sample["exercise_id"]]
+  round.save!
+end
+
+
+
+
+
+
