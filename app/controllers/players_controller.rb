813ad26f7d4e82214254f20493cd2e03c6369a8a
marjorie boyer <marjorie.boyer3@gmail.com>
2018-05-29 19:10:16 +0200

    Creation du screencode + du composant green_band

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,11 +1,8 @@
 class PlayersController < ApplicationController
 
-
   def show
-    # @player = Player.find(params[:id])
-    @exercice = Exercice.joins(rounds: :game).where(rounds: { game_id: params[:game_id] })
-    raise
+    @player = Player.find(params[:id])
+    @exercise = Exercise.joins(rounds: :game).where(rounds: { game_id: params[:game_id] }).first
+    #ne pas oublier de rajouter le "state" lorsque celui-ci sera créé.
   end
-
-
 end
