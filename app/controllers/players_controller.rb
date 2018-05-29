b8d1b2d529480308830789812900117a0b76fc19
marjorie boyer <marjorie.boyer3@gmail.com>
2018-05-29 15:10:13 +0200

    Create dashboard

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,11 @@
 class PlayersController < ApplicationController
+
+
+  def show
+    # @player = Player.find(params[:id])
+    @exercice = Exercice.joins(rounds: :game).where(rounds: { game_id: params[:game_id] })
+    raise
+  end
+
+
 end
