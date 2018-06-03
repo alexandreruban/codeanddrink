629738feb291625836439e9dd8061e7673f4b115
Guillaume Legrand <gjlegrandfr@gmail.com>
2018-06-03 15:43:50 +0200

    fix welcome and navbar

--- app/controllers/game_master/games_controller.rb
+++ app/controllers/game_master/games_controller.rb
@@ -2,4 +2,8 @@ class GameMaster::GamesController < ApplicationController
   def index
     @games = Game.where(game_master: current_game_master)
   end
+  def show
+    @game = Game.find(params[:id])
+    @players = @game.players
+  end
 end
