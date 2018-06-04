e78603bf5f536b7aeeca4dd66f4ea3b7933b2e8a
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-06-04 12:30:09 +0200

    action cable setup

--- app/javascript/components/editor.js
+++ app/javascript/components/editor.js
@@ -2,41 +2,45 @@ import "ace-builds";
 import "ace-builds/webpack-resolver";
 
 function codescreen() {
-  const editor = ace.edit("editor");
-  editor.setTheme('ace/theme/monokai');
-  editor.session.setMode("ace/mode/ruby");
-  editor.commands.addCommand({
-    name: 'myCommand',
-    bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
-    exec: function(editor) {
-        // NOP
-      },
-    readOnly: true // false if this command should not apply in readOnly mode
-  });
+  const ed = document.getElementById('editor');
+  if (ed) {
+    const editor = ace.edit("editor");
+    editor.setTheme('ace/theme/monokai');
+    editor.session.setMode("ace/mode/ruby");
+    editor.commands.addCommand({
+      name: 'myCommand',
+      bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
+      exec: function(editor) {
+          // NOP
+        },
+      readOnly: true // false if this command should not apply in readOnly mode
+    });
 
-  const form = document.getElementById("new_attempt");
-  const input = document.getElementById("attempt_player_input");
-  const submit = document.getElementById("submit");
-  const reset = document.getElementById("reset");
-  const template = document.getElementById("template");
-  const last_template = document.getElementById("last_attempt");
-  if (submit) {
-    submit.addEventListener('click', (event) => {
-      event.preventDefault();
-      input.value = editor.getValue();
-      form.submit();
-    });
-  }
-  if (reset) {
-    reset.addEventListener('click', (event) => {
-      event.preventDefault();
-      editor.setValue(template.innerHTML);
-    });
-  }
-  if (last_attempt.innerHTML == "") {
-    editor.setValue(template.innerText);
-  } else {
-    editor.setValue(last_attempt.innerText);
+    const form = document.getElementById("new_attempt");
+    const input = document.getElementById("attempt_player_input");
+    const submit = document.getElementById("submit");
+    const reset = document.getElementById("reset");
+    const template = document.getElementById("template");
+    const last_template = document.getElementById("last_attempt");
+    if (submit) {
+      submit.addEventListener('click', (event) => {
+        event.preventDefault();
+        input.value = editor.getValue();
+        form.submit();
+      });
+    }
+    if (reset) {
+      reset.addEventListener('click', (event) => {
+        event.preventDefault();
+        editor.setValue(template.innerHTML);
+      });
+    }
+    if (last_attempt.innerHTML == "") {
+      editor.setValue(template.innerText);
+    } else {
+      editor.setValue(last_attempt.innerText);
+    }
   }
 }
+
 export {codescreen};
